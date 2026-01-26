7b64ffef139e45a8e1d6f2a7a71fb5a4c1805971
so-dawg <meychansopheaktra@gmail.com>
2026-01-26 22:36:06 +0700

    init commit

--- main/projects/logic/task.java
+++ main/projects/logic/task.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.time.localDate;
 
 enum task_status {
@@ -68,3 +69,35 @@ public class task {
   }
 }
 
+=======
+<<<<<<< HEAD
+package logic;
+
+public class task {
+    member taskMem = new member(null, 0);
+    public static void main(String[] name){
+        System.out.println(taskMem.name);
+    }
+    
+}
+=======
+public class task {
+  int deadline;
+  int project_id;
+  enum status;
+  enum priority;
+  String title;
+  String assigned_to;
+
+  public addTask(String title, String assigned_to, enum priority, enum status, int project_id, int deadline){
+    this.title = title;
+    this.assigned_to = assigned_to;
+    this.status = status;
+    this.priority = priority;
+    this.deadline = deadline;
+    this.project_id = project_id;
+  }
+  
+}
+>>>>>>> d9cac3c (init commit)
+>>>>>>> 56dc88a (init commit)
