aeb408405bb5585b48ceec30be1aa3dc5bc001aa
sokhamuny <sokhamuny.keav@gmail.com>
2026-01-27 12:54:26 +0700

    write deadline

--- main/projects/logic/deadline.java
+++ main/projects/logic/deadline.java
@@ -1,14 +1,40 @@
-// Java program to demonstrate
-// LocalDate.now() method
+package logic;
+public class deadline {
 
-import java.time.*;
+    //Basic attributes
+    private int id;
+    private String title;
+    private String duedate;
+    private boolean completed;
+    // Constructor
+    public deadline(int id, String title, String dueDate) {
+        this.id = id;
+        this.title = title;
+        this.duedate = dueDate;
+        this.completed = false;
+    }
+    //Mark as completed
+    public void markCompleted(){
+        completed = true;
+    }
+    //check if complete
+    public boolean isCompleted(){
+        return completed;
+    }
+    // Get information
+    public String getInfo() {
+        return "ID: " + id +
+               "\nTitle: " + title +
+               "\nDue Date: " + duedate +
+               "\nCompleted: " + completed;
+    }
 
-public class main {
-    public static void main(String[] args)
-    {
+    // Simple setters
+    public void setTitle(String title) {
+        this . title = title;
+    }
 
-        LocalDate lt = LocalDate.now();
-
-        System.out.println(lt);
+    public void setDueDate(String dueDate) {
+        this. duedate  = dueDate;
     }
 }
