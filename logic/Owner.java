4c147b3f32849f2fed37a8f6d401f3dc3934cd6a
HENG368 <songhengkim93@gmail.com>
2026-02-24 10:48:23 +0700

    add deadline in Owner

--- logic/Owner.java
+++ logic/Owner.java
@@ -1,28 +1,33 @@
-import java.util.Scanner;
-import logic.Task;
-import java.time.localDate;
-public class Owner{ 
 
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+public class Owner{ 
     private int Id;
-    private int day;
-    private int month;
-    private int year;
+    private LocalDate date;
+    private LocalTime date_time;
     private String task;
     private String membername;
-    private String email;
     private boolean isowner = true;
     
-    public Owner(int Id, String task, String membername, String email) {
+    
+    public Owner(int Id, String task, String membername, String email, LocalDate date, LocalTime time) {
         this.Id = Id;
-        this.task = task;;
-        this.membername = membername;
-        this.email = email;
+        this.task = task;
+        this.membername = membername;  
+        this.date = date;
+        this.date_time = time;
+        isowner = true;
     }
     private Scanner sc = new Scanner(System.in);
     // this task is for owner giving task to member. it need to give deadline ,id member and we need to get task from Task.java 
     // getter section
-    public String get_Deadline() {
-        return deadline;
+    public LocalDate get_Date() {
+        return date;
+    }
+    public LocalTime get_Time() {
+        return date_time;
     }
     public int get_Id(){
         return Id;
@@ -33,34 +38,38 @@ public class Owner{
     public String get_name(){
         return membername;
     }
-    public String get_email(){
-        return email;
-    }
     //for deadline section 
 
-    public int set_deadline(String deadline) {
-        System.out.print("Enter deadline (Ex: DD-MM-YYYY):");
-        String deadline = sc.nextLine();
-        String pattern = "^\\d{2}-\\d{2}-\\d{4}$";
-        if (deadline.matches(pattern)) {
-          String[] tokens = deadline.split("-");
-          for (String token : tokens){
-            int[] values = Integer.parseInt(token.trim());
-          }
-          int days = values[0];
-          int months = values[1];
-          int years = values[2];
-          if (!Task.isPastDeadline(years, months, days)){
-            
-          }
-        }
-        System.out.println("Invalid deadline format! Please use DD-MM-YYYY.");
-        return this.deadline;
+    public void set_deadline() {
+        while (true) {
+            // in put deadline and time
+            System.out.print("Enter deadline (Ex: DD-MM-YYYY):");
+            String deadline = sc.nextLine();
+            System.out.println("Enter time (Ex: HH:MM):");
+            String time = sc.nextLine();
 
+            //parttern for deadline and time
+            String pattern_dedline = "^\\d{2}-\\d{2}-\\d{4}$";
+            String pattern_time = "^\\d{2}:\\d{2}$";
+
+            if(deadline.isEmpty() || time.isEmpty()) {
+                System.err.println("Deadline and time cannot be empty!");
+                continue;
+            }
+            try {
+                this.date = LocalDate.parse(deadline,DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                this.date_time = LocalTime.parse(time);
+                return;
+            } 
+            catch (Exception e) {
+                System.err.println("Invalid date or time format!");
+            }
+        }
+        
     }
 
     
-    //for id section
+    //for search id section
     private int input_Id(){
         System.out.print("Enter member ID:");
         int id = sc.nextInt();
@@ -99,29 +108,29 @@ public class Owner{
         }
         return true;
     }
-    public void request_join(Owner member){
-        boolean ver_email =  member.isValidEmail(member.get_email());
-        if(ver_email){
-            System.out.println("name:" + member.get_name() + "\nID:" + member.get_Id() + "\nEmail:" + member.get_email());
-            System.out.println("this member should approve or not?(yes/no)");
-            String answer = sc.nextLine();
-            if (answer.equalsIgnoreCase("yes")) {
-                System.out.println("Request approved.");
-            } else if (answer.equalsIgnoreCase("no")) {
-                System.out.println("Request denied.");
-            } else {
-                System.out.println("Invalid input. Please enter 'yes' or 'no'.");
-            }
-        }
-        else{
-            System.out.println("This invalid email.");
-        }
+    // public void request_join(Owner member){
+    //     boolean ver_email =  member.isValidEmail(member.get_email());
+    //     if(ver_email){
+    //         System.out.println("name:" + member.get_name() + "\nID:" + member.get_Id() + "\nEmail:" + member.get_email());
+    //         System.out.println("this member should approve or not?(yes/no)");
+    //         String answer = sc.nextLine();
+    //         if (answer.equalsIgnoreCase("yes")) {
+    //             System.out.println("Request approved.");
+    //         } else if (answer.equalsIgnoreCase("no")) {
+    //             System.out.println("Request denied.");
+    //         } else {
+    //             System.out.println("Invalid input. Please enter 'yes' or 'no'.");
+    //         }
+    //     }
+    //     else{
+    //         System.out.println("This invalid email.");
+    //     }
         
-    } 
+    // } 
 
-    void sc_close() {
-        sc.close();
-    }
+    // void sc_close() {
+    //     sc.close();
+    // }
     
     // //for task section
     // void taskby_owner(){ 
@@ -154,19 +163,16 @@ public class Owner{
 // tring() {
     //         return "Owner setting completed.";
     //     }
-    @Override
-    public String toString() {
-        return "Owner setting completed.";
-    }
+
+    // @Override
+    // public String toString() {
+    //     return "Owner setting completed.";
+    // }
 
 public static void main(String[] args) {
-        Owner o = new Owner("", 0, "add instructions", "heng", "heng@example.com");
-        String deadline = o.inputDeadline();
-        int search_ID = o.input_Id();
-        o.set_deadline(deadline);
-        System.out.println("Deadline set to: " + o.set_deadline(deadline));
-        o.Find_member(o, search_ID);
+        
     }
 
 
 }
+
